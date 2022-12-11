0bcb8b242a30ca58b8961fa6827969f2f452d731
conors-code <conor_darcy@hotmail.com>
2022-12-11 20:04:09 +0000

    Altered .sol constructor, 1st real test, +mappings

--- contracts/ChristmasClub.sol
+++ contracts/ChristmasClub.sol
@@ -5,18 +5,29 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract ChristmasClub {
-    uint public unlockTime;
+    ///@notice future from now, matching 01/Dec of the year. 
+    uint256 public unlockStartTime;
+    
+    ///@notice future from now, matching very end of 31/Dec of the year.
+    //If now is > unlockEndTime, then we're in the next year so bump forward
+    //both unlockStartTime and unlockEndTime by one year.
+    uint public unlockEndTime;
+    
     address payable public owner;
+
+    mapping (address => uint256) saverAmounts;
 
     event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
+    event Deposit(uint amount, uint when);
+
+    constructor(uint256 _unlockStartTime) payable {
         require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
+            block.timestamp < _unlockStartTime,
+            "Unlock start time should be in the future"
         );
 
-        unlockTime = _unlockTime;
+        unlockStartTime = _unlockStartTime;
         owner = payable(msg.sender);
     }
 
@@ -24,15 +35,23 @@ contract ChristmasClub {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        require(block.timestamp >= unlockStartTime, "You can't withdraw yet");
+        //require(msg.sender == owner, "You aren't the owner");
 
         emit Withdrawal(address(this).balance, block.timestamp);
 
         owner.transfer(address(this).balance);
     }
 
-    function anything() public pure returns (uint256 blah) {
-        return 256;
+    function deposit(uint256 amount) public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+
+        require(block.timestamp <= unlockStartTime, "Too late to make another deposit - you can withdraw now");
+        require(msg.sender == owner, "You aren't the owner");
+
+        emit Deposit(address(this).balance, block.timestamp);
+
+        saverAmounts[msg.sender] += amount;
     }
 }
