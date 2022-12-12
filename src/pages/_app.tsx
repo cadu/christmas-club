7d3dc9439a28f00baa2a3eb6e56dd2445793d296
cadu <cadu@2km.com.br>
2022-12-12 15:10:10 -0300

    add connectkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,29 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
-import Layout from "../components/Layout";
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import Layout from "../components/Layout";
+import { WagmiConfig, createClient } from "wagmi";
+import { mainnet, goerli } from "wagmi/chains";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
+
+const client = createClient(
+  getDefaultClient({
+    appName: "Christmas Club",
+    infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    chains: [mainnet, goerli],
+  })
+);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
 };
 
