7c8090a59220a4a1c22fec5973dde6afb36811b7
cadu <cadu@2km.com.br>
2022-12-14 17:26:44 -0300

    add read contract

--- src/components/ContractTotals.tsx
+++ src/components/ContractTotals.tsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "hardhat";
-import ChristmasClub from '../artifacts/contracts/ChristmasClub.sol/ChristmasClub.json';
-import { useAccount, useContractRead } from 'wagmi';
+import { BigNumber } from "ethers";
+import ChristmasClub from "../artifacts/contracts/ChristmasClub.sol/ChristmasClub.json";
+import { useAccount, useContractRead } from "wagmi";
 import { useIsMounted } from "../hooks/useIsMounted";
 
 const ContractTotals = (props) => {
-  
   const [numberOfSavers, setNumberOfSavers] = useState(0);
-  // Call smart contract, fetch current value 
-  
-  const { address, status, isConnected} = useAccount();
- 
- 
-  const { data, isError, isLoading } = useContractRead({
+  // Call smart contract, fetch current value
+
+  const { address, status, isConnected } = useAccount();
+
+  const {
+    data: getXdata,
+    isError,
+    isLoading,
+  } = useContractRead({
     address: process.env.NEXT_PUBLIC_CC_CONTRACT_ADDRESS,
     abi: ChristmasClub.abi,
-    functionName: 'getX',
+    functionName: "getX",
+    // onSuccess(data) {
+    //   data = BigNumber.from(data).toString();
+    // },
   });
 
-  const mounted = useIsMounted(); 
+  const mounted = useIsMounted();
   /*
   async function fetchContractTotals() {
 
@@ -34,20 +40,24 @@ const ContractTotals = (props) => {
   }
   }*/
 
-
-  useEffect(() => {
-    if (!data) return;
-    console.log(data);
-    //setNumberOfSavers(data);
-  });
- //inside the return ( ... ) it's the HTML world.  Outside of return( ... ) it's TS / javascript world. 
+  // useEffect(() => {
+  //   if (!getXdata) return;
+  //   console.log(getXdata);
+  //   setNumberOfSavers(Number(getXdata));
+  // }, [getXdata]);
+  if (!mounted) {
+    return null;
+  }
+  //inside the return ( ... ) it's the HTML world.  Outside of return( ... ) it's TS / javascript world.
   return (
     <div>
-    <h1>Hi Cadu!</h1> 
-    { mounted && (
-          `Savers:  user wallet ${address} status ${status} Total Amount Saved {}  Total Goal {}`)
-    }
-    
+      <h1>Hi Cadu!</h1>
+
+      <p>
+        Savers: user wallet ${address} status ${status} Total Amount Saved {}{" "}
+        Total Goal
+      </p>
+      <div>{Number(getXdata)}</div>
     </div>
   );
 };
