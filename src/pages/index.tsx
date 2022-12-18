d2312477eca7519af4b08531fd23937ff1a27c9b
cadu <cadu@2km.com.br>
2022-12-18 20:25:46 -0300

    fix: duplicate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,6 @@ import Deposit from "../components/Deposit";
 import Withdraw from "../components/Withdraw";
 import SaverBalance from "../components/SaverBalance";
 import SetGoal from "../components/SetGoal";
-import Player from "../components/Player";
-import ChristmasTree from "../components/ChristmasTree";
-import Hero from "../components/Hero";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -40,7 +37,6 @@ const Home: NextPage = () => {
         <SetGoal />
         <Deposit />
         <Withdraw />
-        <SetGoal />
       </main>
     </>
   );
