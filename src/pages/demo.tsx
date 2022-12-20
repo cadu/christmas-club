ca1a89aaeed7a891801241654636d89fe23bde3d
cadu <cadu@2km.com.br>
2022-12-19 23:00:04 -0300

    feature: user disconnected

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -2,20 +2,31 @@ import type { NextPage } from "next";
 import MintSavingsToken from "../components/MintSavingsToken";
 import SaverBalance from "../components/SaverBalance";
 import InWithdrawalPeriod from "../components/InWithdrawalPeriod";
+import { useAccount } from "wagmi";
+import { ConnectKitButton } from "connectkit";
 
 const Demo: NextPage = () => {
+  const { isConnected } = useAccount();
   return (
     <main className="container mx-auto flex flex-col max-w-4xl pt-6">
       <div className="flex justify-between items-start">
         <div className="flex flex-col">
           <h2 className="font-bold text-2xl pb-6">Welcome to the demo page.</h2>
-          <p>Don't have USDC? We got you covered!</p>
-          <p className="pb-6">Click the button bellow and get $ 100 :)</p>
-          <MintSavingsToken />
+          {isConnected && (
+            <>
+              <p>Don&apos;t have USDC? We got you covered!</p>
+              <p className="pb-6">Click the button bellow and get $ 100 :)</p>
+            </>
+          )}
+          {isConnected ? (
+            <MintSavingsToken />
+          ) : (
+            <ConnectKitButton label="Connect your wallet to enjoy the Christmas Club" />
+          )}
         </div>
-        <SaverBalance />
+        {isConnected && <SaverBalance />}
       </div>
-      <InWithdrawalPeriod />
+      {isConnected && <InWithdrawalPeriod />}
     </main>
   );
 };
