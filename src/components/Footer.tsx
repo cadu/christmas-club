bec18232df997f421cf1ac3b84c138e4fff66e83
cadu <cadu@2km.com.br>
2022-12-21 14:28:47 -0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,8 @@ import Link from "next/link";
 const Footer = (props: { className: string }) => {
   return (
     <footer className={props.className}>
-      <Link href="https://example.com/#contact">
-        <a target="_blank">Contact</a>
-      </Link>
-      <Link href="https://example.com/security">
-        <a target="_blank">Security</a>
-      </Link>
-      <Link href="/faq">
-        <a>FAQ</a>
+      <Link href="https://christmas-club.vercel.app/">
+        <a>Christmas Club</a>
       </Link>
     </footer>
   );
